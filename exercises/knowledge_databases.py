1ad5ee9226f48fc5696e5932094033b69741d0fa
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 14:12:31 +0300

    dsukhcsdk

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,8 +8,16 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(Article_name,Article_topic,Article_rating):
+	knowledge_object= Knowledge(
+		Article_name=Article_name,
+		Article_topic=Article_topic,
+		Article_rating=Article_rating)
+	session.add(knowledge_object)
+	session.commit()
+
+add_article("rainbow","weather", 9)
+	
 
 def query_all_articles():
 	pass
@@ -25,3 +33,4 @@ def delete_all_articles():
 
 def edit_article_rating():
 	pass
+
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
@@ -13,5 +14,29 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__="knowledge"
+	knowledge_id= Column(Integer, primary_key=True)
+	Article_name=Column(String)
+	Article_topic=Column(String)
+	Article_rating=Column(Integer)
 
-	pass
+	def __repr__(self):
+		return("Knowledge Article_name:{}\n"
+			"Knowledge Article_topic:{}\n"
+			"Knowledge Article_rating:{}").format(
+			self.Article_name,
+			self.Article_topic,
+			self.Article_rating)
+
+		print(repr(Knowledge.__table__))
+
+
+s=Knowledge(Article_name="rainbow",Article_topic="weather",Article_rating=9)
+print(s)
+
+
+
+
+
+
+	

cbb13fcce8c3669bc18b663fb6f6a5a70e6ca1f0
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 10:33:37 +0300

    error bc we delete all the articles before we search

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -20,17 +20,41 @@ add_article("rainbow","weather", 9)
 	
 
 def query_all_articles():
-	pass
+	articles= session.query(
+		Knowledge).all()
+	return articles
+# print(query_all_articles())
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(a1):
+	topic=session.query(
+		Knowledge).filter_by(
+		Article_name=a1).first()
+	return topic
+# print(query_article_by_topic("s"))
+	
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(Article_name):
+	session.query(Knowledge).filter_by(
+		Article_name=Article_name).delete()
+	session.commit()
+# delete_article_by_topic("rainbow")
 
-def delete_all_articles():
-	pass
+	
 
-def edit_article_rating():
-	pass
+def delete_all_articles(Article_name):
+	session.query(Knowledge).delete()
+	session.commit()
+delete_all_articles("Knowledge")
+	
+
+def edit_article_rating(update_rating,article_title):
+	food_object=session.query(
+		Knowledge).filter_by(
+		Article_topic=article_title).first()
+	food_object.Article_rating=update_rating
+	session.commit()
+
+edit_article_rating(5,"weather")
+
+	
 
